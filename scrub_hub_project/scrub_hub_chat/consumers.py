f28fed4f69a16511486cd278a138db5ebf4408c6
VictorGrayDallas <VictorGrayDallas@gmail.com>
2024-04-22 01:14:52 -0500

    Improve real-time messaging, primarily by adding username and time to messages and letting the user type in a message.

--- scrub_hub_project/scrub_hub_chat/consumers.py
+++ scrub_hub_project/scrub_hub_chat/consumers.py
@@ -1,6 +1,8 @@
 # Channels (the library handling async connections) "consumers"
 # analagous to Django's "views"
 
+import datetime
+
 from channels.generic.websocket import JsonWebsocketConsumer
 from asgiref.sync import async_to_sync
 from channels.layers import InMemoryChannelLayer
@@ -29,18 +31,48 @@ class GroupedConsumer(JsonWebsocketConsumer):
 			async_to_sync(self.channel_layer.group_discard)(group, self.channel_name)
 
 class ChatConsumer(GroupedConsumer):
+	"""
+	Each user will be placed in a "group" that is only for that user.
+	This group will make it so that a user can have multiple tabs open and see notifications on all of them.
+	We do not have a group for the current conversation. Maybe this will change at some point.
+	"""
+
+	def get_user_id(self):
+		# Auth isn't implemented yet
+		# self.scope['user'] ... TODO
+		ChatConsumer.mock_user_id += 1
+		self.user_id = ChatConsumer.mock_user_id
+
+	def get_users_in_conversation(self, conversation_id):
+		# Nothing in the database is set up yet. TODO
+		users = []
+		for id in range(1, ChatConsumer.mock_user_id + 1):
+			if id == self.user_id:
+				continue
+			users.append(str(id))
+		return users
+
+
 	def connect(self):
-		self.room_name = 'room1'
-		self.group_add(self.room_name)
+		self.get_user_id()
+		self.group_add(str(self.user_id))
 		self.accept()
 
 	def receive_json(self, data):
 		message = {
 			'message': data['message'],
+			'username': f'Guest user {self.user_id}',
+			'time': str(datetime.datetime.now(datetime.UTC)),
 			'type': 'chat_message', # Tells channels what method to use to handle the group message.
 		}
-		self.group_send(self.room_name, message)
+		# TODO: Add message to database
+
+		# Forward the message to each user in the conversation
+		for id in self.get_users_in_conversation(0):
+			self.group_send(id, message)
 
 	# This is a handler for "group_send". group_send will get and call a function with the name of the message's "type" value.
 	def chat_message(self, event):
-		self.send_json({ 'message': event['message'] })
+		del event['type']
+		self.send_json(event)
+ChatConsumer.mock_user_id = 0
--- scrub_hub_vite/src/Components/ChatComponent.tsx
+++ scrub_hub_vite/src/Components/ChatComponent.tsx
@@ -1,20 +1,56 @@
 import { useState } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+// TODO: Have actually good styles.
+import styles from './ChatComponent.module.css'; // VS Code extension "CSS Modules" by clinyong gives autocomplete support for css modules
+
+type MessageProps = {
+	message: string,
+	username: string,
+	time: Date,
+};
+/**
+ * Checks if the given object is a MessageProps.
+ * If it is a MessageProps except that time is a string (the way it is received from the server), it converts time to a Date.
+ * @param obj The object to check and convert.
+ */
+function isMessageProps(obj: Partial<MessageProps>): obj is MessageProps {
+	const canBeMessageProps = !!(
+		obj && obj.message && obj.username && obj.time &&
+		typeof obj.message === 'string' &&
+		typeof obj.username === 'string'
+	);
+	if (canBeMessageProps && typeof obj.time === 'string')
+		obj.time = new Date(obj.time as string);
+	return canBeMessageProps;
+}
+function MessageComponent({ message, username, time} : MessageProps) {
+	return <>
+		<text className={styles.messageUsername}>{username}</text>
+		<text className={styles.messageTime}>{time.toLocaleString()}</text>
+		<p className={styles.message}>{message}</p>
+	</>
+}
 
 export type ChatProps = {
 	participants: string[],
 };
 export function ChatComponent({ participants }: ChatProps) {
-	const [messages, ] = useState<string[]>([]);
+	const [messages, ] = useState<MessageProps[]>([]);
+	const addMessage = (nessage: MessageProps) => {
+		messages.push(nessage);
+	}
+
 	const { readyState, sendJsonMessage } = useWebSocket(`ws://${window.location.host}/ws/`,
 		{
 			onOpen: () => console.log('open'),
 			onClose: () => console.log('close'),
 			onMessage: (event) => {
 				const data = JSON.parse(event.data);
-				if (data.message && typeof data.message === 'string') {
-					messages.push(data.message);
-					console.log(data.message);
+				if (isMessageProps(data)) {
+					addMessage(data);
+				} else {
+					console.log('not a message props');
+					console.log(data);
 				}
 			},
 			shouldReconnect: (_) => false,
@@ -29,12 +65,34 @@ export function ChatComponent({ participants }: ChatProps) {
 		[ReadyState.UNINSTANTIATED]: "Uninstantiated",
 	}[readyState];
 
-	return <>
-		<h2>{participants.join(', ')}</h2>
+	const [userMessage, setMessageInput] = useState('');
+	const send = () => {
+		if (!userMessage)
+			return;
+
+		const message: MessageProps = {
+			message: userMessage,
+			username: 'You',
+			time: new Date(),
+		};
+		sendJsonMessage(message);
+		addMessage(message);
+		setMessageInput('');
+	};
+
+	// Any tag selectors in the .module.css file would apply to the entire page.
+	// We get around this by giving the component a "root" element.
+	// Selector "button" changes to ".root button" and now only buttons inside this component are styled.
+	return <div className={styles.root}>
+		<h2>Participants: {participants.join(', ')}</h2>
 		<p>Status: {connectionStatus}</p>
-		<button type='button' onClick={() => sendJsonMessage({message: 'hillo'})}>SEND</button>
-		{messages.map((m, i) => <div key={i}>{m}</div>)}
-	</>
+		{messages.map((m, i) => <MessageComponent key={i} {...m} />)}
+		<input id='messagebox' type='text' placeholder='message' value={userMessage}
+			onChange={(e) => setMessageInput(e.target.value)}
+			onKeyUp={(e) => { if (e.key === 'Enter') send() }}
+		/>
+		<button type='button' onClick={send}>SEND</button>
+	</div>
 }
 
 export default ChatComponent;
