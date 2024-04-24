b1f5941dac0c21903d6d8d1aedac82543dac1abc
JohanLee2000 <94586878+JohanLee2000@users.noreply.github.com>
2024-04-23 21:44:34 -0500

    clean up files

--- scrub_hub_vite/vite.config.ts
+++ scrub_hub_vite/vite.config.ts
@@ -5,16 +5,16 @@ import react from '@vitejs/plugin-react'
 // NOTE: This file is not used in dev mode and probably doesn't really need to even exist right now.
 export default defineConfig({
 	plugins: [react()],
-	base: '/static/', //FOR PRODUCTION Uncomment
-	// build: {
-	// 	outDir: '../scrub_hub_project/vite_dist', // place our output files in django directory
-	// 	manifest: 'manifest.json', // required for django_vite
-	// 	emptyOutDir: true,
-	// 	rollupOptions: {
-	// 		input: {
-	// 			src: '/src/main.tsx',
-	// 		}
-	// 	}
-	// }
+	base: '/static/',
+	build: {
+		outDir: '../scrub_hub_project/vite_dist', // place our output files in django directory
+		manifest: 'manifest.json', // required for django_vite
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				src: '/src/main.tsx',
+			}
+		}
+	}
 
 })
