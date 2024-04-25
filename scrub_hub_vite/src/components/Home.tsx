9eb5dc1517fc2d0665fa35601b0ac1c269c4f09b
JohanLee2000 <94586878+JohanLee2000@users.noreply.github.com>
2024-04-25 15:56:43 -0500

    homepage configuration with images

--- scrub_hub_vite/src/components/Home.tsx
+++ scrub_hub_vite/src/components/Home.tsx
@@ -1,16 +1,23 @@
+import scrubHubLogo from "../assets/scrubHubLogo.png"
+import scrubPeople from "../assets/scrubPeople.png"
 
 const Home = () => {
 	return(
-		<div>
-		<h2>Home</h2>
-		<div>
-			<p>
-				<a href='/authenticate/login'>Login Here</a>
-			</p>
-			<p>
-				<a href='/authenticate/register'>Register here.</a>
-			</p>
-		</div>
+		<div style={{ display: "flex", justifyContent: "space-between" }}>
+			<div style={{ flex: 1 }}>
+				<img src={scrubHubLogo} alt="Scrub Hub Logo" />
+				<h2>Streamline Communication and Collaboration for Medical Practitioners</h2>
+				<div>
+				<p>
+					Join our platform and experience seamless communication and collaboration among medical professionals.
+				</p>
+				<button style={{ marginRight: '10px' }} onClick={() => window.location.href = '/authenticate/login'}>Login Here</button>
+				<button onClick={() => window.location.href = '/authenticate/register'}>Register here</button>
+				</div>
+			</div>
+			<div style={{ flex: 1 }}>
+				<img src={scrubPeople} alt="Scrub People" />
+			</div>
 		</div>
 	)
 }
