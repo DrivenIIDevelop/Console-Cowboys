77ed4e5542caaec33666c8f7b1ba9f0a0a5c7334
JohanLee2000 <94586878+JohanLee2000@users.noreply.github.com>
2024-04-28 18:37:42 -0500

    home page customized to look like wireframe

--- scrub_hub_vite/src/components/Home.tsx
+++ scrub_hub_vite/src/components/Home.tsx
@@ -4,20 +4,24 @@ import "../index.css"
 
 const Home = () => {
 	return(
-		<div className="flex justify-between">
-			<div className="ml-16">
-				<img className="justify-center" src={scrubHubLogo} alt="Scrub Hub Logo" />
-				<h2 className="font-fira-condensed font-bold text-[48px]">Streamline <br></br>Communication and<br></br> Collaboration for <br></br>Medical Practitioners</h2>
-				<div>
-				<p className="font-fira-condensed text-[26px] mt-5 mb-5">
-					Join our platform and experience seamless<br></br> communication and collaboration among medical<br></br> professionals.
-				</p>
-				<button className=" mr-10" onClick={() => window.location.href = '/authenticate/login'}>Login</button>
-				<button onClick={() => window.location.href = '/authenticate/register'}>Register</button>
+		<div className="flex h-screen">
+			<div className="flex-1 flex flex-col">
+				<div className="max-w-sm mx-auto justify-center mt-10">
+					<img className="w-[260px] h-[260px]" src={scrubHubLogo} alt="Scrub Hub Logo" />
+				</div>
+				<div className="ml-[110px]">
+					<h2 className="font-fira-condensed font-bold text-[48px]">Streamline <br></br>Communication And<br></br> Collaboration For <br></br>Medical Practitioners</h2>
+					<div>
+					<p className="font-fira-condensed text-[26px] mt-5 mb-8">
+						Join our platform and experience seamless<br></br> communication and collaboration among medical<br></br> professionals.
+					</p>
+					<button className="bg-[#00AEB5] text-white py-2 px-6 rounded-md mr-8" onClick={() => window.location.href = '/authenticate/login'}>Login</button>
+					<button className="bg-[#D8D8D9] text-black py-2  px-6 rounded-md" onClick={() => window.location.href = '/authenticate/register'}>Register</button>
+					</div>
 				</div>
 			</div>
-			<div>
-				<img className= "" src={scrubPeople} alt="Scrub People" />
+			<div className="flex-1">
+				<img className= "w-full h-full object-cover" src={scrubPeople} alt="Scrub People" />
 			</div>
 		</div>
 	)
