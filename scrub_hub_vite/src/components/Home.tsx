42bb058f759ec4eb9b6f830e47a719ca55e1b7c7
JohanLee2000 <94586878+JohanLee2000@users.noreply.github.com>
2024-04-28 17:01:42 -0500

    configure tailwindCSS for vite

--- scrub_hub_vite/src/components/Home.tsx
+++ scrub_hub_vite/src/components/Home.tsx
@@ -1,21 +1,22 @@
 import scrubHubLogo from "../assets/scrubHubLogo.png"
 import scrubPeople from "../assets/scrubPeople.png"
+import "../index.css"
 
 const Home = () => {
 	return(
-		<div style={{ display: "flex", justifyContent: "space-between" }}>
-			<div style={{ flex: 1 }}>
+		<div className="flex justify-between columns-2">
+			<div>
 				<img src={scrubHubLogo} alt="Scrub Hub Logo" />
 				<h2>Streamline Communication and Collaboration for Medical Practitioners</h2>
 				<div>
-				<p>
+				<p className="text-red-500">
 					Join our platform and experience seamless communication and collaboration among medical professionals.
 				</p>
 				<button style={{ marginRight: '10px' }} onClick={() => window.location.href = '/authenticate/login'}>Login</button>
 				<button onClick={() => window.location.href = '/authenticate/register'}>Register</button>
 				</div>
 			</div>
-			<div style={{ flex: 1 }}>
+			<div>
 				<img src={scrubPeople} alt="Scrub People" />
 			</div>
 		</div>
