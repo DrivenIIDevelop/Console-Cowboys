d86a83bfd736c79b96476c128d78dc405c97b89f
JohanLee2000 <94586878+JohanLee2000@users.noreply.github.com>
2024-05-05 10:15:04 -0500

    edit dashboard

--- scrub_hub_vite/src/components/Dashboard.tsx
+++ scrub_hub_vite/src/components/Dashboard.tsx
@@ -1,12 +1,14 @@
 import Cookies from 'universal-cookie';
+import doctors from "../assets/doctors.png";
 
 const cookies = new Cookies();
 
 type DashboardProps = {
-  first_name: string
+  first_name: string;
+  last_name: string;
 }
 
-const Dashboard = ({ first_name }: DashboardProps) => {
+const Dashboard = ({ first_name, last_name }: DashboardProps) => {
 
   //Logout the user
   const handleLogout = async () => {
@@ -32,12 +34,17 @@ const Dashboard = ({ first_name }: DashboardProps) => {
   };
 
   return (
-    <div>
-      <h2>Dashboard</h2>
-        <div>
-          <p>Welcome, {first_name}!</p>
+    <div className="">
+      <div className="flex justify-center border-width-[15px] rounded-lg bg-[#80CED7] px-10 py-10">
+        <div className="flex-1 flex-col flex">
+          <p className="text-[24px]">Welcome Back!</p>
+          <p className="text-[32px] text-white">Dr. {first_name} {last_name}</p>
           <button onClick={handleLogout}>Logout</button>
         </div>
+        <div className="flex-1">
+          <img className="w-full h-full" src={doctors} alt="doctors"/>
+        </div>
+      </div>
     </div>
   );
 };
