75d97069d457d57531ef5b6280bcd6d90cdb8366
VictorGrayDallas <VictorGrayDallas@gmail.com>
2024-05-05 13:38:05 -0500

    Add functions to generate RSA key pair with encrypted private key, and to decrypt private key baesd on password.

---
+++ scrub_hub_vite/src/encryption.ts
@@ -0,0 +1,40 @@
+import OpenCrypto from 'opencrypto'
+
+const encoder = new TextEncoder();
+const salt = encoder.encode('ppkpdsaltscrubhub');
+
+const rsaParams: RsaHashedKeyGenParams = {
+	name: 'RSA-OAEP',
+	hash: 'SHA-256',
+	modulusLength: 4096,
+	publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+}
+
+/**
+ * Generates a RSA key pair, and uses the given password to encrypt the private key.
+ */
+export async function generateKeys(password: string) {
+	// can't be used to encrypt, only derive
+	const pair = await crypto.subtle.generateKey(rsaParams, true, ['encrypt', 'decrypt']);
+	const publicKey = crypto.subtle.exportKey('spki', pair.publicKey);
+
+	// I was unable to use wrapKey to encrypt the private key.
+	// While investigating why, I found this library that does the job.
+	const oc = new OpenCrypto();
+	const privateKey: Promise<string> = oc.encryptPrivateKey(pair.privateKey, password + salt, 100000, 'SHA-256', 'AES-GCM', 256);
+
+	return {
+		public: await publicKey,
+		private: await privateKey,
+	}
+}
+
+export async function decryptPrivateKey(key: string, password: string): Promise<CryptoKey> {
+	const oc = new OpenCrypto();
+	return await oc.decryptPrivateKey(key, password + salt, {
+		name: 'RSA-OAEP',
+		hash: 'SHA-256',
+		usages: ['decrypt', 'unwrapKey'],
+		isExtractable: true
+	});
+}
