f174ff870640c9f6c2bdb5d4baf4bb7ca2976444
JohanLee2000 <94586878+JohanLee2000@users.noreply.github.com>
2024-05-14 16:31:06 -0500

    dashboard and top nav bar

--- scrub_hub_vite/src/components/Dashboard.tsx
+++ scrub_hub_vite/src/components/Dashboard.tsx
@@ -1,5 +1,6 @@
 import Cookies from 'universal-cookie';
-import doctors from "../assets/doctors.png";
+import doctors from '../assets/doctors.png';
+import email from '../assets/email.png';
 
 const cookies = new Cookies();
 
@@ -14,13 +15,13 @@ const Dashboard = ({ first_name, last_name }: DashboardProps) => {
   const handleLogout = async () => {
     try
     {
-      const response = await fetch("/authenticate/logout/", {
-        method: "POST",
+      const response = await fetch('/authenticate/logout/', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
-          "X-CSRFToken": cookies.get("csrftoken"),
+          'Content-Type': 'application/json',
+          'X-CSRFToken': cookies.get('csrftoken'),
         },
-        credentials: "same-origin",
+        credentials: 'same-origin',
       });
       if (response.ok) {
         window.location.href = `${location.protocol}//${location.host}/authenticate/login`;
@@ -34,19 +35,59 @@ const Dashboard = ({ first_name, last_name }: DashboardProps) => {
   };
 
   return (
-    <div className="">
-      <div className="flex justify-center border-width-[15px] rounded-lg bg-[#80CED7] px-10 py-10">
-        <div className="flex-1 flex-col flex">
-          <p className="text-[24px]">Welcome Back!</p>
-          <p className="text-[32px] text-white">Dr. {first_name} {last_name}</p>
-          <button onClick={handleLogout}>Logout</button>
+    <div className='absolute top-[100px] left-[105px] right-0 bottom-0'>
+      <div className='w-full h-full flex bg-[#EEE]'>
+        <div className='w-2/3 ml-5 mt-5'>
+          <div className='inline-flex justify-end h-[220px] w-full pl-[70px] pr-[112px] rounded-[15px] bg-[#80CED7]'>
+            <div className='flex-1 flex-col flex justify-center'>
+              <div className='-translate-y-4'>
+                <p className='text-[22px]'>Welcome Back!</p>
+                <p className='text-[32px] text-white'>Dr. {first_name} {last_name}</p>
+              </div>
+              
+              <div className='px-12'>
+                <button className='bg-gray-100' onClick={handleLogout}>Logout</button>
+              </div>
+              
+            </div>
+            <div className='flex-1'>
+              <img className='w-full h-full' src={doctors} alt='Doctors'/>
+            </div>
+          </div>
+          <div className='flex h-1/3 mt-5'>
+            <div className='w-1/2 bg-gray-200'>
+              Recent Conversations
+            </div>
+            <div className='w-1/2 ml-5 bg-slate-300'>
+              Email Status
+              <img className='' src={email} alt='Email'/>
+            </div>
+          </div>
+          <div className='h-1/3 mt-5 overflow-y-auto bg-orange-500'>
+            Previous Patients
+          </div>
+
         </div>
-        <div className="flex-1">
-          <img className="w-full h-full" src={doctors} alt="doctors"/>
+        <div className='w-1/4 mt-5 ml-5 bg-[#63C7B2] border rounded-tl-[15px]'>
+          Calendar
         </div>
       </div>
     </div>
+    
   );
 };
+
+{/* <div className=''>
+      <div className='inline-flex h-[200px] pr-[112px] pl-[443px] justify-end items-center flex-shrink-0 rounded-[15px] bg-[#80CED7]'>
+        <div className='flex-1 flex-col flex'>
+          <p className='text-[24px]'>Welcome Back!</p>
+          <p className='text-[32px] text-white'>Dr. {first_name} {last_name}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+        <div className='flex-1'>
+          <img className='w-full h-full' src={doctors} alt='doctors'/>
+        </div>
+      </div>
+    </div> */}
 
 export default Dashboard;
